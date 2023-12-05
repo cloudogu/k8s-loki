5054f7c73b382d4d3ed13a7ae1da990ceeabb3ff
Niklas <niklas.hinze@cloudogu.com>
2023-12-05 09:53:51 +0100

    #2 Update component patch templates in release process.

---
+++ release_args.sh
@@ -0,0 +1,80 @@
+#!/bin/bash
+set -o errexit
+set -o nounset
+set -o pipefail
+
+componentTemplateFile=k8s/helm/component-patch-tpl.yaml
+lokiTempChart="/tmp/loki"
+lokiTempValues="${lokiTempChart}/values.yaml"
+lokiTempChartYaml="${lokiTempChart}/Chart.yaml"
+lokiValues="k8s/helm/values.yaml"
+
+# this function will be sourced from release.sh and be called from release_functions.sh
+update_versions_modify_files() {
+  echo "Update helm dependencies"
+  make helm-update-dependencies  > /dev/null
+
+  # Extract loki chart
+  local lokiVersion
+  lokiVersion=$(yq '.dependencies[] | select(.name=="loki").version' < "k8s/helm/Chart.yaml")
+  local lokiPackage
+  lokiPackage="k8s/helm/charts/loki-${lokiVersion}.tgz"
+
+  echo "Extract loki helm chart"
+  tar -zxvf "${lokiPackage}" -C "/tmp" > /dev/null
+
+  local lokiAppVersion
+  lokiAppVersion=$(yq '.appVersion' < "${lokiTempChartYaml}")
+
+  echo "Set images in component patch template"
+
+  local lokiKubectlRegistry
+  local lokiKubectlRepo
+  local lokiKubectlTag
+  lokiKubectlRegistry=$(yq '.loki.kubectlImage.registry' < "${lokiValues}")
+  lokiKubectlRepo=$(yq '.loki.kubectlImage.repository' < "${lokiValues}")
+  lokiKubectlTag=$(yq '.loki.kubectlImage.tag' < "${lokiValues}")
+  setAttributeInComponentPatchTemplate ".values.images.loki.kubectl" "${lokiKubectlRegistry}/${lokiKubectlRepo}:${lokiKubectlTag}"
+
+  local lokiImageRegistry
+  local lokiImageRepo
+  lokiImageRegistry=$(yq '.loki.image.registry' < "${lokiTempValues}")
+  lokiImageRepo=$(yq '.loki.image.repository' < "${lokiTempValues}")
+  setAttributeInComponentPatchTemplate ".values.images.loki.image" "${lokiImageRegistry}/${lokiImageRepo}:${lokiAppVersion}"
+
+  local lokiCanaryRegistry
+  local lokiCanaryRepo
+  lokiCanaryRegistry=$(yq '.loki.monitoring.lokiCanary.image.registry' < "${lokiTempValues}")
+  lokiCanaryRepo=$(yq '.loki.monitoring.lokiCanary.image.repository' < "${lokiTempValues}")
+  setAttributeInComponentPatchTemplate ".values.images.loki.image" "${lokiImageRegistry}/${lokiImageRepo}:${lokiAppVersion}"
+
+  local lokiGatewayRegistry
+  local lokiGatewayRepo
+  local lokiGatewayTag
+  lokiGatewayRegistry=$(yq '.gateway.image.registry' < "${lokiTempValues}")
+  lokiGatewayRepo=$(yq '.gateway.image.repository' < "${lokiTempValues}")
+  lokiGatewayTag=$(yq '.gateway.image.tag' < "${lokiTempValues}")
+  setAttributeInComponentPatchTemplate ".values.images.gateway.image" "${lokiGatewayRegistry}/${lokiGatewayRepo}:${lokiGatewayTag}"
+
+  local lokiSidecarRegistryRepo
+  local lokiSidecarTag
+  lokiSidecarRegistryRepo=$(yq '.sidecar.image.repository' < "${lokiTempValues}")
+  lokiSidecarTag=$(yq '.sidecar.image.tag' < "${lokiTempValues}")
+  setAttributeInComponentPatchTemplate ".values.images.sidecar.image.registryRepo" "${lokiSidecarRegistryRepo}"
+  setAttributeInComponentPatchTemplate ".values.images.sidecar.image.tag" "${lokiSidecarTag}"
+
+  rm -rf ${lokiTempChart}
+}
+
+setAttributeInComponentPatchTemplate() {
+  local key="${1}"
+  local value="${2}"
+
+  yq -i "${key} = \"${value}\"" "${componentTemplateFile}"
+}
+
+update_versions_stage_modified_files() {
+  git add "${componentTemplateFile}"
+}
+
+update_versions_modify_files
